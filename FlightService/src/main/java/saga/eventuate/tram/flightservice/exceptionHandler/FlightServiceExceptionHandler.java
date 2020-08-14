9271934c197f4a5327aeb89f29f27bd198e63070
KarolinDuerr <karolin.duerr@hotmail.com>
2020-08-14 21:45:30 +0200

    merged ExceptionHandlers in one class

--- FlightService/src/main/java/saga/eventuate/tram/flightservice/exceptionHandler/FlightServiceExceptionHandler.java
+++ FlightService/src/main/java/saga/eventuate/tram/flightservice/exceptionHandler/FlightServiceExceptionHandler.java
@@ -6,6 +6,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.server.ResponseStatusException;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 import saga.eventuate.tram.flightservice.error.ErrorMessage;
 import saga.eventuate.tram.flightservice.error.ErrorType;
@@ -19,6 +20,19 @@ public class FlightServiceExceptionHandler extends ResponseEntityExceptionHandle
         ErrorMessage errorMessage = new ErrorMessage(exception.getErrorType(), exception.getMessage());
         return handleExceptionInternal(exception, errorMessage, new HttpHeaders(),
                 mapErrorTypeToResponseStatus(exception.getErrorType()), webRequest);
+    }
+
+    @ExceptionHandler(value = ResponseStatusException.class)
+    protected ResponseEntity<Object> handle(ResponseStatusException exception, WebRequest webRequest) {
+        throw exception;
+    }
+
+    @ExceptionHandler(value = Exception.class)
+    protected ResponseEntity<Object> handle(Exception exception, WebRequest webRequest) {
+        logger.error("An exception occurred: " + exception.getMessage() + ", of type " + exception.getClass() + " " +
+                "with the following cause: " + exception.getCause());
+        return handleExceptionInternal(exception, "There has been an error, please check your input and try again.",
+                new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR, webRequest);
     }
 
     private HttpStatus mapErrorTypeToResponseStatus(ErrorType errorType) {
--- FlightService/src/main/java/saga/eventuate/tram/flightservice/model/Location.java
+++ FlightService/src/main/java/saga/eventuate/tram/flightservice/model/Location.java
@@ -6,7 +6,7 @@ public class Location {
 
     private String city;
 
-    public Location() {
+    private Location() {
 
     }
 
