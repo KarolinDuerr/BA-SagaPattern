43cb648e03f50270c099e7d47a7cbf085ab5b13c
KarolinDuerr <karolin.duerr@hotmail.com>
2020-11-15 21:52:12 +0100

    moved Conductor Beans in ConductorConfiguration class for FlightService

--- FlightService/src/main/java/saga/netflix/conductor/flightservice/controller/FlightServiceConfiguration.java
+++ FlightService/src/main/java/saga/netflix/conductor/flightservice/controller/FlightServiceConfiguration.java
@@ -1,23 +1,16 @@
 package saga.netflix.conductor.flightservice.controller;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.netflix.conductor.client.http.MetadataClient;
-import com.netflix.conductor.client.http.TaskClient;
-import com.netflix.conductor.common.utils.JsonMapperProvider;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 import saga.netflix.conductor.flightservice.model.FlightInformationRepository;
 import saga.netflix.conductor.flightservice.resources.DtoConverter;
 
 @Configuration
 @EnableAutoConfiguration
+@Import(ConductorConfiguration.class)
 public class FlightServiceConfiguration {
-
-
-    @Value("${conductor.server.uri}")
-    String conductorServerUri;
 
     @Bean
     public IFlightService flightService(FlightInformationRepository flightInformationRepository) {
@@ -27,41 +20,5 @@ public class FlightServiceConfiguration {
     @Bean
     public DtoConverter dtoConverter() {
         return new DtoConverter();
-    }
-
-    @Bean
-    public ObjectMapper objectMapper() {
-        ObjectMapper objectMapper = new JsonMapperProvider().get();
-        return objectMapper;
-    }
-
-    @Bean
-    public TaskClient taskClient() {
-        TaskClient taskClient = new TaskClient();
-        taskClient.setRootURI(conductorServerUri);
-        return taskClient;
-    }
-
-    @Bean
-    public MetadataClient metaDataClient() {
-        MetadataClient metadataClient = new MetadataClient();
-        metadataClient.setRootURI(conductorServerUri);
-        return metadataClient;
-    }
-
-    @Bean
-//    @Retryable(value = {SocketException.class, RuntimeException.class, ConductorClientException.class}, maxAttempts = 10, backoff = @Backoff(delay = 20000))
-    public WorkerDispatcher workerDispatcher(TaskClient taskClient, MetadataClient metadataClient,
-                                             ObjectMapper objectMapper, IFlightService flightService,
-                                             DtoConverter dtoConverter) {
-        try {
-            Thread.sleep(65000); // TODO
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        WorkerDispatcher createdWorkerDispatcher = new WorkerDispatcher(taskClient, metadataClient, objectMapper,
-                flightService, dtoConverter);
-        createdWorkerDispatcher.startTaskPolling();
-        return createdWorkerDispatcher;
     }
 }
