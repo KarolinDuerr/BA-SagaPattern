3e3095e336ef08e782b2b5c78280f08af9ec3f75
KarolinDuerr <karolin.duerr@hotmail.com>
2020-11-30 23:04:27 +0100

    added automated generation of an OpenAPI documentation to EventuateTram project

---
+++ EventuateTram/FlightService/src/main/java/saga/eventuate/tram/flightservice/controller/OpenApiConfiguration.java
@@ -0,0 +1,19 @@
+package saga.eventuate.tram.flightservice.controller;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.servers.Server;
+import org.springframework.context.annotation.Configuration;
+
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Hotel Service",
+                description = "An example for a Hotel service that enables the user to see already booked hotels.",
+                version = "1.0.0",
+                contact = @Contact(url = "https://github.com/KarolinDuerr/BA-SagaPattern", name = "Karolin Dürr")),
+        servers = @Server(url = "http//localhost:8081")
+)
+@Configuration
+public class OpenApiConfiguration {
+}
