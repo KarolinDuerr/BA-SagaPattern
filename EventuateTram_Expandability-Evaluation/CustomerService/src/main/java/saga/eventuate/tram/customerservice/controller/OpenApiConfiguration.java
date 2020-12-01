23d949c0300375295efd059b1489a1a785345e7c
KarolinDuerr <karolin.duerr@hotmail.com>
2020-12-01 01:16:14 +0100

    some corrections in OpenApiConfiguration files

--- EventuateTram_Expandability-Evaluation/CustomerService/src/main/java/saga/eventuate/tram/customerservice/controller/OpenApiConfiguration.java
+++ EventuateTram_Expandability-Evaluation/CustomerService/src/main/java/saga/eventuate/tram/customerservice/controller/OpenApiConfiguration.java
@@ -3,6 +3,7 @@ package saga.eventuate.tram.customerservice.controller;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Contact;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.context.annotation.Configuration;
 
 @OpenAPIDefinition(
@@ -12,7 +13,8 @@ import org.springframework.context.annotation.Configuration;
                 version = "1.0.0",
                 contact = @Contact(
                         url = "https://github.com/KarolinDuerr/BA-SagaPattern/tree/master/EventuateTram_Expandability-Evaluation",
-                        name = "Karolin Dürr"))
+                        name = "Karolin Dürr")),
+        servers = @Server(url = "http://localhost:8083")
 )
 @Configuration
 public class OpenApiConfiguration {
