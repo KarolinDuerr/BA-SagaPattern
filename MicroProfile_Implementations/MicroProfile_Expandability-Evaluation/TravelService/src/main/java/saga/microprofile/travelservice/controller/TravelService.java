b350f2e3327058d01864ca773347e591b8281aa7
KarolinDuerr <karolin.duerr@hotmail.com>
2021-06-19 19:55:26 +0200

    MicroProfile projects: remove unnecessary ThreadFactory from TravelService

--- MicroProfile_Implementations/MicroProfile_Expandability-Evaluation/TravelService/src/main/java/saga/microprofile/travelservice/controller/TravelService.java
+++ MicroProfile_Implementations/MicroProfile_Expandability-Evaluation/TravelService/src/main/java/saga/microprofile/travelservice/controller/TravelService.java
@@ -4,14 +4,12 @@ import saga.microprofile.travelservice.error.*;
 import saga.microprofile.travelservice.model.*;
 import saga.microprofile.travelservice.saga.*;
 
-import javax.annotation.Resource;
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 import java.net.URI;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Optional;
-import java.util.concurrent.ThreadFactory;
 import java.util.logging.Logger;
 
 @ApplicationScoped
@@ -19,9 +17,6 @@ import java.util.logging.Logger;
 public class TravelService implements ITravelService {
 
     private static final Logger logger = Logger.getLogger(TravelService.class.toString());
-
-    @Resource(lookup = "concurrent/threadFactory2")
-    private ThreadFactory threadFactory;
 
     @Inject
     @LraCoordinatorServiceImpl
